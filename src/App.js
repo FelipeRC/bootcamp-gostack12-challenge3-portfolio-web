97711c02e2c8fceb91974605f0008ae04d8f8b65
Felipe <feliperosacruz@gmail.com>
2020-06-18 17:32:13 +0100

    Adding functionality to list, create and remove repositories.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => {
+        console.log(response);
+
+        setRepositories(response.data);
+      });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const {status, data} = await api.post('/repositories', {
+      "title": "Portfolio Project",
+      "url": "https://github.com/FelipeRC/bootcamp-gostack12-challenge3-portfolio-web",
+      "techs": ["ReactJS"]
+    });
+    
+    if (status && status === 201) {
+      setRepositories([...repositories, data]);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    if(id){
+      const {status} = await api.delete(`/repositories/${id}`);
+
+      if(status && status === 204){
+        setRepositories(repositories.filter(repository => repository.id !== id));
+      }
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
           </button>
-        </li>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
